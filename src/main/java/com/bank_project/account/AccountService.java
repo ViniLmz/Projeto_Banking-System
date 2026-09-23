24424a76ae11446a913383867a5857ae5e535351
Vini <vinicius.lima.silva25@hotmail.com>
2026-09-23 16:07:11 -0300

    Test Unitario jnuit

--- src/main/java/com/bank_project/account/AccountService.java
+++ src/main/java/com/bank_project/account/AccountService.java
@@ -1,11 +1,13 @@
 package com.bank_project.account;
 
+import com.bank_project.exception.AccountNumberAlreadyExistsException;
+import com.bank_project.exception.BlockedAccountException;
 import com.bank_project.custumer.Custumer;
 import com.bank_project.custumer.CustumerRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.bank_project.exception.ResourceNotFoundException;
-
+import com.bank_project.exception.InsufficientBalanceException;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
@@ -13,7 +15,10 @@ import java.util.Optional;
 @Service
 public class AccountService {
 
+    @Mock
     private final AccountRepository accountRepository;
+
+    @Mock
     private final CustumerRepository custumerRepository;
 
 
@@ -33,6 +38,10 @@ public class AccountService {
 
 
     public Account save(AccountRequest request) {
+
+        if (accountRepository.existsByAccountNumber(request.accountNumber())) {
+            throw new AccountNumberAlreadyExistsException("Account number already exists");
+        }
         Account account = new Account(
                 null,
                 request.accountNumber(),
@@ -41,27 +50,36 @@ public class AccountService {
                 request.status()
         );
 
-        if (request.customerId() != null) {
-            Custumer custumer = custumerRepository.findById(request.customerId())
-                    .orElseThrow(() -> new ResourceNotFoundException("Customer not found with id: " + request.customerId()));
-            account.setCustomer(custumer);
-        }
+        Custumer custumer = custumerRepository.findById(request.customerId())
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found with id: " + request.customerId()));
+        account.setCustomer(custumer);
 
         return accountRepository.save(account);
     }
 
+    @Transactional
     public void delete(Long id) {
+
+        if (!accountRepository.existsById(id)) {
+            throw new ResourceNotFoundException(
+                    "Account not found with id: " + id
+            );
+        }
+
         accountRepository.deleteById(id);
     }
 
     @Transactional
-    public Account update(Long id, Account account) {
-        if (accountRepository.existsById(id)) {
-            account.setId(id);
-            return accountRepository.save(account);
-        } else {
-            throw new ResourceNotFoundException("Account not found with id: " + id);
-        }
+    public Account update(Long id, AccountUpdateRequest request) {
+
+        Account account = accountRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Account not found with id: " + id
+                ));
+
+        account.setAccountType(request.accountType());
+
+        return account;
     }
 
 
@@ -69,6 +87,10 @@ public class AccountService {
 
         Account account = accountRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Account not found with id: " + id));
+
+        if (account.getStatus() == AccountStatus.BLOCKED) {
+            throw new BlockedAccountException("This account is currently blocked");
+        }
 
         account.setBalance(
                 account.getBalance().add(request.amount())
@@ -81,18 +103,33 @@ public class AccountService {
 
         Account account = accountRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Account not found with id: " + id));
+
+        if (account.getStatus() == AccountStatus.BLOCKED) {
+            throw new BlockedAccountException("This account is currently blocked");
+        }
+
         if (request.amount().compareTo(account.getBalance()) <= 0) {
             account.setBalance(
                     account.getBalance().subtract(request.amount())
             );
         } else {
-            throw new RuntimeException("Insufficient balance");
-
+            throw new InsufficientBalanceException("Insufficient balance");
         }
 
         return accountRepository.save(account);
     }
 
+    @Transactional
+    public Account updateStatus(Long id, AccountStatus status) {
+
+        Account account = accountRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Account not found with id: " + id));
+
+        account.setStatus(status);
+
+        return accountRepository.save(account);
+    }
 
 }
 
